d23dd5e531547c7ac286b19613681cf42ef344cf
SJLEE316 <lsj8249@naver.com>
2021-01-16 13:52:19 +0900

    day5 supplementary explanation by comments

--- day5/greeting.js
+++ day5/greeting.js
@@ -7,13 +7,14 @@ const USER_LS = "currentUser_apple", //소스 검사에서 나오는 값.
   SHOWING_CN = "showing";
 
 function paintGreeting(text) {
+  // value값이 파라미터로 들어간다
   form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
+  greeting.classList.add(SHOWING_CN); //js-greetings showing -> display:block
+  greeting.innerText = `Hello ${text}`; // 'Hello [설정한 value값]
 }
 
 function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
+  const currentUser = localStorage.getItem(USER_LS); // value값 저장
   // localStorage.setItem("lucy", "true") 하면 lucy는 key, true는 value로 저장된다.
   // lovalStorage.getItem("lucy") 하면 true값이 나온다.
   if (currentUser === null) {
