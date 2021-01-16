be498b9336bbc5590584121364b246c230180ee5
SJLEE316 <lsj8249@naver.com>
2021-01-16 14:27:30 +0900

    day6

--- day5/greeting.js
+++ day5/greeting.js
@@ -6,6 +6,22 @@ const USER_LS = "currentUser_apple", //소스 검사에서 나오는 값.
   // 예를 들어 const USER_LC = "apple"이면 소스 검사에 apple이라고 뜬다.
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // enter 적용안됨
+  const currentValue = input.value;
+  paintGreeting(currentValue); // 불러오기만 할 뿐 저장되지는 않는다
+  saveName(currentValue); // 저장한다
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   // value값이 파라미터로 들어간다
   form.classList.remove(SHOWING_CN);
@@ -19,6 +35,7 @@ function loadName() {
   // lovalStorage.getItem("lucy") 하면 true값이 나온다.
   if (currentUser === null) {
     // she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
